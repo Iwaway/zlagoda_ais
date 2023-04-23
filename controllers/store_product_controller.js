ce2368465598ea519e830241c0e0b1297056f3c8
Iwaway <belokonegor2000@gmail.com>
2023-04-24 00:40:43 +0300

    added authorized validation

---
+++ controllers/store_product_controller.js
@@ -0,0 +1,125 @@
+const { pool } = require("../db");
+
+const getAll = (request, response) => {
+    pool.query('SELECT * FROM product ORDER BY product_name ASC', (error, results) => {
+        if (error) {
+            response.status(500).send(error.message)
+            console.log(error.message)
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getAllByCategory = (request, response) => {
+    const {
+        category_number
+    } = request.body
+    if (!category_number) {
+        res.status(400).json({message: "Bad Request: category number is mandatory"})
+    }
+    pool.query('SELECT * FROM product WHERE category_number = $1 ORDER BY product_name ASC', [category_number], (error, results) => {
+        if (error) {
+            response.status(500).send(error.message)
+            console.log(error.message)
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getByName = (request, response) => {
+    const {
+        name
+    } = request.body
+    if (!name) {
+        res.status(400).json({message: "Bad Request: name is mandatory"})
+    }
+    pool.query('SELECT * FROM product WHERE product_name = $1 ORDER BY product_name ASC', [name], (error, results) => {
+        if (error) {
+            response.status(500).send(error.message)
+            console.log(error.message)
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const getById = (request, response) => {
+    const id = parseInt(request.params.id)
+    if (!id) {
+        res.status(400).json({message: "Bad Params: id is mandatory"})
+    }
+    pool.query('SELECT * FROM product WHERE id_product = $1', [id], (error, results) => {
+        if (error) {
+            response.status(500).send(error.message)
+            console.log(error.message)
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const create = (request, response) => {
+    const {
+        name,
+        category_number,
+        characteristics
+    } = request.body
+    if (!name || !category_number || !characteristics) {
+        res.status(400).json({message: "Bad Request: name, category number, characteristics is mandatory"})
+    }
+    pool.query('INSERT INTO product (category_number, product_name, characteristics) VALUES ($2, $1, $3)',
+    [name, category_number, characteristics], (error, results) => {
+        if (error) {
+            response.status(500).send(error.message)
+            console.log(error.message)
+        }
+        response.status(201).send(`Product added with name: ${name}`)
+    })
+}
+
+const update = (request, response) => {
+    const id = parseInt(request.params.id)
+    if (!id) {
+        res.status(400).json({message: "Bad Params: id is mandatory"})
+    }
+    const {
+        name,
+        category_number,
+        characteristics
+    } = request.body
+    if (!name || !category_number || !characteristics) {
+        res.status(400).json({message: "Bad Request: name, category number, characteristics is mandatory"})
+    }
+    pool.query(
+        'UPDATE product SET name = $1, category_number = $2, characteristics = $3 WHERE id_product = $4',
+        [name, category_number, characteristics, id], (error, results) => {
+            if (error) {
+                response.status(500).send(error.message)
+                console.log(error.message)
+            }
+            response.status(200).send(`Product modified with ID: ${id}`)
+        }
+    )
+}
+
+const deleteById = (request, response) => {
+    const id = parseInt(request.params.id)
+    if (!id) {
+        res.status(400).json({message: "Bad Params: id is mandatory"})
+    }
+    pool.query('DELETE FROM product WHERE id_product = $1', [id], (error, results) => {
+        if (error) {
+            response.status(500).send(error.message)
+            console.log(error.message)
+        }
+        response.status(200).send(`Product with ID: ${id}`)
+    })
+}
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    deleteById,
+    getAllByCategory,
+    getByName,
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,87 +1,142 @@
 const express = require('express');
 const routes = express.Router();
-const employee_controller = require('../controllers/employee_controller');
-const role_controller = require('../controllers/role_controller');
-const category_controller = require('../controllers/category_controller');
-const product_controller = require('../controllers/product_controller');
-const customer_controller = require('../controllers/customer_controller');
-const auth_controller = require('../controllers/auth_controller');
+const employee = require('../controllers/employee_controller');
+const role = require('../controllers/role_controller');
+const category = require('../controllers/category_controller');
+const product = require('../controllers/product_controller');
+const store_product = require('../controllers/store_product_controller');
+const customer = require('../controllers/customer_controller');
+const auth = require('../controllers/auth_controller');
 
 const API_ROOT = '/zlagoda/api'
 
 //- - - - - - - - - - Employees endpoints - - - - - - - - - -
 
 //Отримати інформацію про усіх працівників, відсортованих за прізвищем
-routes.get(API_ROOT + '/employees', auth_controller.authorizeManager, employee_controller.getAll)
+routes.get(API_ROOT + '/employees', auth.authorizeManager, employee.getAll)
 
 //Отримати інформацію про усіх працівників, що обіймають посаду касира, відсортованих за прізвищем
-routes.get('/employees/cashiers', employee_controller.getAllCashiers)
+routes.get('/employees/cashiers', employee.getAllCashiers)
 
 //Додавати, редагувати, видаляти дані про працівників
-routes.get('/employee/:id', employee_controller.getById)
-routes.post('/employee/create', employee_controller.create)
-routes.put('/employee/update/:id', employee_controller.update)
-routes.delete('/employee/delete/:id', employee_controller.deleteById)
+routes.get('/employee/:empl_id', auth.authorizeManager, employee.getById)
+routes.post('/employee/create', auth.authorizeManager, employee.create)
+routes.put('/employee/update/:empl_id', auth.authorizeManager, employee.update)
+routes.delete('/employee/delete/:empl_id', auth.authorizeManager, employee.deleteById)
 
 //За прізвищем працівника знайти його телефон та адресу
-routes.get('/employeeNumberAndAddress', employee_controller.getNumberAndAddress)
+routes.get('/employeeNumberAndAddress', auth.authorizeManager, employee.getNumberAndAddress)
 
 
 //- - - - - - - - - - Roles endpoints - - - - - - - - - -
 
-//Додавати, редагувати, видаляти дані про ролі
-routes.get('/roles', role_controller.getAll)
-routes.get('/role/:id', role_controller.getById)
+//Переглядати дані про ролі
+routes.get('/roles', role.getAll)
+routes.get('/role/:role_id', role.getById)
 
 
 //- - - - - - - - - - Categories endpoints - - - - - - - - - -
 
 //Отримати інформацію про усі категорії, відсортовані за назвою
-routes.get('/categories', category_controller.getAll)
+routes.get('/categories', auth.authorizeManager, category.getAll)
 
 //Додавати, редагувати, видаляти дані про категорії товарів
-routes.get('/category/:id', category_controller.getById)
-routes.post('/category/create', category_controller.create)
-routes.put('/category/update/:id', category_controller.update)
-routes.delete('/category/delete/:id', category_controller.deleteById)
+routes.get('/category/:category_number', auth.authorizeManager, category.getById)
+routes.post('/category/create', auth.authorizeManager, category.create)
+routes.put('/category/update/:category_number', auth.authorizeManager, category.update)
+routes.delete('/category/delete/:category_number', auth.authorizeManager, category.deleteById)
 
 
 //- - - - - - - - - - Product endpoints - - - - - - - - - -
 
 //Отримати інформацію про усі товари, відсортовані за назвою
-routes.get('/products', product_controller.getAll)
+routes.get('/products', auth.authorizeCashierOrManager, product.getAll)
 
 //Здійснити пошук усіх товарів, що належать певній категорії, відсортованих за назвою
-routes.get('/productsByCategory', product_controller.getAllByCategory)
+routes.get('/productsByCategory', auth.authorizeCashierOrManager, product.getAllByCategory)
 
 //Здійснити пошук товарів за назвою
-routes.get('/productsByName', product_controller.getByName)
+routes.get('/productsByName', auth.authorizeCashier, product.getByName)
 
 //Додавати, редагувати, видаляти дані про товари
-routes.get('/product/:id', product_controller.getById)
-routes.post('/product/create', product_controller.create)
-routes.put('/product/update/:id', product_controller.update)
-routes.delete('/product/delete/:id', product_controller.deleteById)
+routes.get('/product/:id_product', auth.authorizeManager, product.getById)
+routes.post('/product/create', auth.authorizeManager, product.create)
+routes.put('/product/update/:id_product', auth.authorizeManager, product.update)
+routes.delete('/product/delete/:id_product', auth.authorizeManager, product.deleteById)
 
 
 //- - - - - - - - - - Customer card endpoints - - - - - - - - - -
 
 //Отримати інформацію про усіх постійних клієнтів, відсортованих за прізвищем
-routes.get('/customers', customer_controller.getAll)
+routes.get('/customers', auth.authorizeCashierOrManager, customer.getAll)
 
 //Отримати інформацію про усіх постійних клієнтів, що мають карту клієнта із певним відсотком, посортованих за прізвищем
-routes.get('/customersByPercent', customer_controller.getAllByPercent)
+routes.get('/customersByPercent', auth.authorizeManager, customer.getAllByPercent)
 
-//Отримати інформацію про усіх постійних клієнтів, що мають карту клієнта із певним відсотком, посортованих за прізвищем
-routes.get('/customerBySurname', customer_controller.getBySurname)
+//Отримати інформацію про усіх постійних клієнтів за прізвищем
+routes.get('/customerBySurname', auth.authorizeCashier, customer.getBySurname)
 
 //Додавати, редагувати, видаляти дані про товари
-routes.get('/customer/:card_number', customer_controller.getById)
-routes.post('/customer/create', customer_controller.create)
-routes.put('/customer/update/:card_number', customer_controller.update)
-routes.delete('/api/customer/delete/:card_number', customer_controller.deleteById)
+routes.get('/customer/:card_number', auth.authorizeManager, customer.getById)
+routes.post('/customer/create', auth.authorizeCashierOrManager, customer.create)
+routes.put('/customer/update/:card_number', auth.authorizeCashierOrManager, customer.update)
+routes.delete('/api/customer/delete/:card_number', auth.authorizeManager, customer.deleteById)
 
-routes.post('/authenticate', auth_controller.authenticate);
-routes.post('/register', auth_controller.register);
+//- - - - - - - - - - Products in storage endpoints - - - - - - - - - -
+
+//Отримати інформацію про усі товари в магазині, відсортовані за назвою
+routes.get('/storeProducts', auth.authorizeCashier, store_product.getAll)
+
+//Отримати інформацію про усі товари в магазині, відсортовані за кількістю
+routes.get('/storeProducts', auth.authorizeManager, store_product.getAll)
+
+//Додавати, редагувати, видаляти дані про товари у магазині
+routes.get('/storeProduct/:upc', auth.authorizeManager, store_product.getById)
+routes.post('/storeProduct/create', auth.authorizeManager, store_product.create)
+routes.put('/storeProduct/update/:upc', auth.authorizeManager, store_product.update)
+routes.delete('/storeProduct/delete/:upc', auth.authorizeManager, store_product.deleteById)
+
+//- - - - - - - - - - Authentication endpoints - - - - - - - - - -
+
+routes.post('/authenticate', auth.authenticate);
+routes.post('/register', auth.register);
 
 module.exports = routes;
+
+//Manager
+// 1. Додавати нові дані про товари у магазині;
+// 2. Редагувати дані про товари у магазині;
+// 3. Видаляти дані про товари у магазині, чеки;
+// 4. Видруковувати звіти з інформацією про усіх працівників, постійних клієнтів,
+// категорії товарів, товари, товари у магазині, чеки;
+// 10. Отримати інформацію про усі товари у магазині, відсортовані за кількістю;
+// 14. За UPC-товару знайти ціну продажу товару, кількість наявних одиниць товару,
+// назву та характеристики товару;
+// 15. Отримати інформацію про усі акційні товари, відсортовані за кількістю одиниць
+// товару/ за назвою;
+// 16. Отримати інформацію про усі не акційні товари, відсортовані за кількістю
+// одиниць товару/ за назвою;
+// 17. Отримати інформацію про усі чеки, створені певним касиром за певний період
+// часу (з можливістю перегляду куплених товарів у цьому чеку, їх назви, к-сті та ціни);
+// 18. Отримати інформацію про усі чеки, створені усіма касирами за певний період
+// часу (з можливістю перегляду куплених товарів у цьому чеку, їх назва, к-сті та ціни);
+// 19. Визначити загальну суму проданих товарів з чеків, створених певним касиром за
+// певний період часу;
+// 20. Визначити загальну суму проданих товарів з чеків, створених усіма касиром за
+// певний період часу;
+// 21. Визначити загальну кількість одиниць певного товару, проданого за певний
+// період часу.
+
+//Cashier
+// 2. Отримати інформацію про усі товари у магазині, відсортовані за назвою;
+// 7. Здійснювати продаж товарів (додавання чеків);
+// 9. Переглянути список усіх чеків, що створив касир за цей день;
+// 10. Переглянути список усіх чеків, що створив касир за певний період часу;
+// 11. За номером чеку вивести усю інформацію про даний чек, в тому числі
+// інформацію про назву, к-сть та ціну товарів, придбаних в даному чеку.
+// 12. Отримати інформацію про усі акційні товари, відсортовані за кількістю одиниць
+// товару/ за назвою;
+// 13. Отримати інформацію про усі не акційні товарів, відсортовані за кількістю
+// одиниць товару/ за назвою;
+// 14. За UPC-товару знайти ціну продажу товару, кількість наявних одиниць товару.
+// 15. Можливість отримати усю інформацію про себе.
