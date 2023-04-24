e5441e0eaaa410eaaca8f5b72d413903e00b2ac3
Illia Stasiuk <stasiuk.illia@scheidt-bachmann.de>
2023-04-24 20:50:50 +0300

    Move routes to new API root

--- routes/routes.js
+++ routes/routes.js
@@ -16,44 +16,44 @@ const API_ROOT = '/zlagoda/api'
 routes.get(API_ROOT + '/employees', auth.authorizeManager, employee.getAll)
 
 //Отримати інформацію про усіх працівників, що обіймають посаду касира, відсортованих за прізвищем
-routes.get('/employees/cashiers', employee.getAllCashiers)
+routes.get(API_ROOT + '/employees/cashiers', employee.getAllCashiers)
 
 //Додавати, редагувати, видаляти дані про працівників
-routes.get('/employee/:id_employee', auth.authorizeManager, employee.getById)
-routes.post('/employee/create', auth.authorizeManager, employee.create)
-routes.put('/employee/update/:id_employee', auth.authorizeManager, employee.update)
-routes.delete('/employee/delete/:id_employee', auth.authorizeManager, employee.deleteById)
+routes.get(API_ROOT + '/employee/:id_employee', auth.authorizeManager, employee.getById)
+routes.post(API_ROOT + '/employee/create', auth.authorizeManager, employee.create)
+routes.put(API_ROOT + '/employee/update/:id_employee', auth.authorizeManager, employee.update)
+routes.delete(API_ROOT + '/employee/delete/:id_employee', auth.authorizeManager, employee.deleteById)
 
 //За прізвищем працівника знайти його телефон та адресу
-routes.get('/employeeNumberAndAddress', auth.authorizeManager, employee.getNumberAndAddress)
+routes.get(API_ROOT + '/employeeNumberAndAddress', auth.authorizeManager, employee.getNumberAndAddress)
 
 
 //- - - - - - - - - - Roles endpoints - - - - - - - - - -
 
 //Переглядати дані про ролі
-routes.get('/roles', role.getAll)
-routes.get('/role/:role_id', role.getById)
+routes.get(API_ROOT + '/roles', role.getAll)
+routes.get(API_ROOT + '/role/:role_id', role.getById)
 
 
 //- - - - - - - - - - Categories endpoints - - - - - - - - - -
 
 //Отримати інформацію про усі категорії, відсортовані за назвою
-routes.get('/categories', auth.authorizeManager, category.getAll)
+routes.get(API_ROOT + '/categories', auth.authorizeManager, category.getAll)
 
 //Додавати, редагувати, видаляти дані про категорії товарів
-routes.get('/category/:category_number', auth.authorizeManager, category.getById)
-routes.post('/category/create', auth.authorizeManager, category.create)
-routes.put('/category/update/:category_number', auth.authorizeManager, category.update)
-routes.delete('/category/delete/:category_number', auth.authorizeManager, category.deleteById)
+routes.get(API_ROOT + '/category/:category_number', auth.authorizeManager, category.getById)
+routes.post(API_ROOT + '/category/create', auth.authorizeManager, category.create)
+routes.put(API_ROOT + '/category/update/:category_number', auth.authorizeManager, category.update)
+routes.delete(API_ROOT + '/category/delete/:category_number', auth.authorizeManager, category.deleteById)
 
 
 //- - - - - - - - - - Product endpoints - - - - - - - - - -
 
 //Отримати інформацію про усі товари, відсортовані за назвою
-routes.get('/products', auth.authorizeCashierOrManager, product.getAll)
+routes.get(API_ROOT + '/products', auth.authorizeCashierOrManager, product.getAll)
 
 //Здійснити пошук усіх товарів, що належать певній категорії, відсортованих за назвою
-routes.get('/productsByCategory', auth.authorizeCashierOrManager, product.getAllByCategory)
+routes.get(API_ROOT + '/productsByCategory', auth.authorizeCashierOrManager, product.getAllByCategory)
 
 //Здійснити пошук товарів за назвою
 routes.get('/productsByName', auth.authorizeCashier, product.getByName)
@@ -68,50 +68,50 @@ routes.delete('/product/delete/:id_product', auth.authorizeManager, product.dele
 //- - - - - - - - - - Customer card endpoints - - - - - - - - - -
 
 //Отримати інформацію про усіх постійних клієнтів, відсортованих за прізвищем
-routes.get('/customers', auth.authorizeCashierOrManager, customer.getAll)
+routes.get(API_ROOT + '/customers', auth.authorizeCashierOrManager, customer.getAll)
 
 //Отримати інформацію про усіх постійних клієнтів, що мають карту клієнта із певним відсотком, посортованих за прізвищем
-routes.get('/customersByPercent', auth.authorizeManager, customer.getAllByPercent)
+routes.get(API_ROOT + '/customersByPercent', auth.authorizeManager, customer.getAllByPercent)
 
 //Отримати інформацію про усіх постійних клієнтів за прізвищем
-routes.get('/customerBySurname', auth.authorizeCashier, customer.getBySurname)
+routes.get(API_ROOT + '/customerBySurname', auth.authorizeCashier, customer.getBySurname)
 
 //Додавати, редагувати, видаляти дані про товари
-routes.get('/customer/:card_number', auth.authorizeManager, customer.getById)
-routes.post('/customer/create', auth.authorizeCashierOrManager, customer.create)
-routes.put('/customer/update/:card_number', auth.authorizeCashierOrManager, customer.update)
-routes.delete('/api/customer/delete/:card_number', auth.authorizeManager, customer.deleteById)
+routes.get(API_ROOT + '/customer/:card_number', auth.authorizeManager, customer.getById)
+routes.post(API_ROOT + '/customer/create', auth.authorizeCashierOrManager, customer.create)
+routes.put(API_ROOT + '/customer/update/:card_number', auth.authorizeCashierOrManager, customer.update)
+routes.delete(API_ROOT + '/api/customer/delete/:card_number', auth.authorizeManager, customer.deleteById)
 
 //- - - - - - - - - - Products in storage endpoints - - - - - - - - - -
 
 //Отримати інформацію про усі товари в магазині, відсортовані за назвою
-routes.get('/storeProducts', auth.authorizeCashier, store_product.getAllNames)
+routes.get(API_ROOT + '/storeProducts', auth.authorizeCashier, store_product.getAllNames)
 
 //Отримати інформацію про усі акційні товари, відсортовані за кількістю одиниць товару
-routes.get('/storeProducts/promotion', auth.authorizeCashierOrManager, store_product.getAllProm)
+routes.get(API_ROOT + '/storeProducts/promotion', auth.authorizeCashierOrManager, store_product.getAllProm)
 
 //Отримати інформацію про усі неакційні товари, відсортовані за кількістю одиниць товару
-routes.get('/storeProducts/nonPromotion', auth.authorizeCashierOrManager, store_product.getAllNonProm)
+routes.get(API_ROOT + '/storeProducts/nonPromotion', auth.authorizeCashierOrManager, store_product.getAllNonProm)
 
 //Отримати інформацію про усі товари в магазині, відсортовані за кількістю
-routes.get('/storeProducts', auth.authorizeManager, store_product.getAll)
+routes.get(API_ROOT + '/storeProducts', auth.authorizeManager, store_product.getAll)
 
 //За UPC-товару знайти ціну продажу товару, кількість наявних одиниць товару
-routes.get('/storeProduct/:upc', auth.authorizeCashier, store_product.getById)
+routes.get(API_ROOT + '/storeProduct/:upc', auth.authorizeCashier, store_product.getById)
 
 //За UPC-товару знайти ціну продажу товару, кількість наявних одиниць товару, назву та характеристики товару
-routes.get('/storeProduct/:upc', auth.authorizeManager, store_product.getByIdAll)
+routes.get(API_ROOT + '/storeProduct/:upc', auth.authorizeManager, store_product.getByIdAll)
 
 //Додавати, редагувати, видаляти дані про товари у магазині
-routes.get('/storeProduct/:upc', store_product.getById)
-routes.post('/storeProduct/create', auth.authorizeManager, store_product.create)
-routes.put('/storeProduct/update/:upc', auth.authorizeManager, store_product.update)
-routes.delete('/storeProduct/delete/:upc', auth.authorizeManager, store_product.deleteById)
+routes.get(API_ROOT + '/storeProduct/:upc', store_product.getById)
+routes.post(API_ROOT + '/storeProduct/create', auth.authorizeManager, store_product.create)
+routes.put(API_ROOT + '/storeProduct/update/:upc', auth.authorizeManager, store_product.update)
+routes.delete(API_ROOT + '/storeProduct/delete/:upc', auth.authorizeManager, store_product.deleteById)
 
 //- - - - - - - - - - Authentication endpoints - - - - - - - - - -
 
-routes.post('/authenticate', auth.authenticate);
-routes.post('/register', auth.register);
+routes.post(API_ROOT + '/authenticate', auth.authenticate);
+routes.post(API_ROOT + '/register', auth.register);
 
 module.exports = routes;
 
